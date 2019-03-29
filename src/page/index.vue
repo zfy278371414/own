4ce399054da47b6f9723f11b9d1fcbc8f1c03379
Zhongfeiyang <278371414@qq.com>
2019-03-29 17:47:49 +0800

    上传

---
+++ src/page/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        index page
+    </div>
+</template>
+<script>
+export default {
+    created () {
+        this.$api.get('topics',null,r => {
+            console.log(r)
+        })
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Index from '@/page/index'
+import Content from '@/page/content'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      // name: 'HelloWorld',
+      component: Index
+    },
+    {
+      path: '/content/:id',
+      // name: 'HelloWorld',
+      component: Content
+    }
+  ]
+})
